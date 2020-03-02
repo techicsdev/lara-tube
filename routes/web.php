15cca1e6e5848a37e238c5427b0bae3d91d7ace2
techicsdev <ja@sk-associates.org>
2020-03-02 13:54:07 +0600

    a

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,30 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function (Request $request) {
-	// if(!$request->mobile){
- //    	abort(404);
-	// }
+	if(!$request->mobile){
+    	abort(404);
+	}
 	return view('welcome');
 });
 
 Route::get('/page1', function (Request $request) {
-	// if(!$request->mobile){
- //    	abort(404);
-	// }
+	if(!$request->mobile){
+    	abort(404);
+	}
     return view('page1')->with('birthbg',Birthbg::where('type',1)->get());
 });
 
 Route::get('/page2', function (Request $request) {
-	// if(!$request->mobile){
- //    	abort(404);
-	// }
+	if(!$request->mobile){
+    	abort(404);
+	}
     return view('page2')->with('birthbg',Birthbg::where('type',2)->get());
 });
 
 Route::get('/page3', function (Request $request) {
-	// if(!$request->mobile){
- //    	abort(404);
-	// }
+	if(!$request->mobile){
+    	abort(404);
+	}
     return view('page3');
 });
 
