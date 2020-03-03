72190fd7d1e0cb4458ac7ee671a28a0afaac2d55
Syed Iqbal <SyedIqbal@ics.local>
2020-03-03 12:41:07 +0600

    Dispatching event

--- app/Http/Controllers/UploadVideoController.php
+++ app/Http/Controllers/UploadVideoController.php
@@ -23,9 +23,12 @@ class UploadVideoController extends Controller
             'path' => request()->video->store("channels/{$channel->id}")
         ]);
 
-        $this->dispatch(new CreateVideoThumbnail($video));
+        //$this->dispatch(new CreateVideoThumbnail($video));
 
-        $this->dispatch(new ConvertForStreaming($video));
+        CreateVideoThumbnail::dispatch($video);
+        ConvertForStreaming::dispatch($video);
+
+        //$this->dispatch(new ConvertForStreaming($video));
 
         return $video;
     }
